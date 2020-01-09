add1556ddfb5283f87789b4fbf988e20e7812c44
nmendozavd <nmendozavd@gmail.com>
2020-01-09 11:27:24 -0800

    Added tables to schema

--- server/schema.sql
+++ server/schema.sql
@@ -4,11 +4,32 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+
 );
 
-/* Create other tables and define schemas for them here! */
 
+-- CREATE TABLE users (
+--   /* Describe your table here.*/
+--     id INT NOT NULL AUTO_INCREMENT,
+--     name VARCHAR(255) DEFAULT '' NOT NULL,
+--     PRIMARY KEY(id)
+-- );
 
+-- /* Create other tables and define schemas for them here! */
+-- CREATE TABLE messages (
+--     id INT DEFAULT '0000' NOT NULL AUTO_INCREMENT,
+--     message VARCHAR(255) DEFAULT '' NOT NULL,
+--     PRIMARY KEY(id),
+--     FOREIGN KEY (user) REFERENCES users(id),
+--     FOREIGN KEY (room) REFERENCES rooms(id),
+-- );
+
+-- CREATE TABLE rooms (
+--     id INT DEFAULT '0000' NOT NULL AUTO_INCREMENT,
+--     name VARCHAR(255) DEFAULT '' NOT NULL,
+--     PRIMARY KEY(id)
+
+-- );
 
 
 /*  Execute this file from the command line by typing:
