517da787ad72cdb8fdb607158a0728e2b52f83b5
nmendozavd <nmendozavd@gmail.com>
2020-01-09 20:22:30 -0800

    Completed schema sql

--- server/schema.sql
+++ server/schema.sql
@@ -1,38 +1,35 @@
+DROP DATABASE IF EXISTS chat;
 CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE users (
+    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) DEFAULT '' NOT NULL
+);
+
+CREATE TABLE rooms (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) DEFAULT '' NOT NULL
+);
+
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id INT(11) AUTO_INCREMENT PRIMARY KEY,
+  text VARCHAR(255) NOT NULL,
+  room_id INT(11),
+  user_id INT(11),
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (room_id) REFERENCES rooms(id)
 
 );
 
+-- Maybe INNER JOIN on many to many users to rooms ???
+-- SELECT id FROM users, rooms
+-- INNER JOIN
 
--- CREATE TABLE users (
---   /* Describe your table here.*/
---     id INT NOT NULL AUTO_INCREMENT,
---     name VARCHAR(255) DEFAULT '' NOT NULL,
---     PRIMARY KEY(id)
--- );
-
--- /* Create other tables and define schemas for them here! */
--- CREATE TABLE messages (
---     id INT DEFAULT '0000' NOT NULL AUTO_INCREMENT,
---     message VARCHAR(255) DEFAULT '' NOT NULL,
---     PRIMARY KEY(id),
---     FOREIGN KEY (user) REFERENCES users(id),
---     FOREIGN KEY (room) REFERENCES rooms(id),
--- );
-
--- CREATE TABLE rooms (
---     id INT DEFAULT '0000' NOT NULL AUTO_INCREMENT,
---     name VARCHAR(255) DEFAULT '' NOT NULL,
---     PRIMARY KEY(id)
-
--- );
 
 
 /*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
+ *    mysql -u student < server/schema.sql
  *  to create the database and the tables.*/
 
