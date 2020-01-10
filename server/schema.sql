4cde11283bc408774e138bbdf911c90b9097f0ef
nmendozavd <nmendozavd@gmail.com>
2020-01-09 21:36:13 -0800

    Update schema with solutions video version

--- server/schema.sql
+++ server/schema.sql
@@ -5,23 +5,37 @@ USE chat;
 
 CREATE TABLE users (
     id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) DEFAULT '' NOT NULL
+    username VARCHAR(255) DEFAULT '' NOT NULL
 );
 
-CREATE TABLE rooms (
-    id INT(11) AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) DEFAULT '' NOT NULL
-);
-
+-- Solutions Video messages TABLE
 CREATE TABLE messages (
   id INT(11) AUTO_INCREMENT PRIMARY KEY,
   text VARCHAR(255) NOT NULL,
-  room_id INT(11),
+  roomname VARCHAR(255) NOT NULL,
   user_id INT(11),
   FOREIGN KEY (user_id) REFERENCES users(id),
-  FOREIGN KEY (room_id) REFERENCES rooms(id)
 
 );
+
+-- My rooms table
+-- CREATE TABLE rooms (
+--     id INT(11) AUTO_INCREMENT PRIMARY KEY,
+--     name VARCHAR(255) DEFAULT '' NOT NULL
+-- );
+
+
+-- My messages table with room as a foreign key > must also edit models
+
+-- CREATE TABLE messages (
+--   id INT(11) AUTO_INCREMENT PRIMARY KEY,
+--   text VARCHAR(255) NOT NULL,
+--   room_id INT(11),
+--   user_id INT(11),
+--   FOREIGN KEY (user_id) REFERENCES users(id),
+--   FOREIGN KEY (room_id) REFERENCES rooms(id)
+
+-- );
 
 -- Maybe INNER JOIN on many to many users to rooms ???
 -- SELECT id FROM users, rooms
